71ce37685537d14d56ed3c6de373caba57f0e2e5
Pedro Rodrigues <peddroprp@gmail.com>
2021-02-28 12:30:37 -0300

    Add minus, plus and number definitions

--- src/parser/parser.hs
+++ src/parser/parser.hs
@@ -3,7 +3,7 @@ module Parser where
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
-import qualified Text.Parsec.Expr as Expr
+import qualified Text.Parsec.Expr as Ex
 import qualified Text.Parsec.Token as Token
 
 -- Definição da linguagem e suas regras
@@ -26,11 +26,23 @@ envLang = Token.LanguageDef{
 lexerTokens :: Token.TokenParser ()
 lexerTokens = Token.makeTokenParser envLang
 
-operatorsReserved :: Parser -> Parser ()
-bracket = Token.operatorsReserved lexerTokens
+operatorsReserved :: String -> Parser ()
+operatorsReserved = Token.reservedOp lexerTokens
 
 langReserved :: String -> Parser ()
-bracket = Token.langReserved lexerTokens
+langReserved = Token.reserved lexerTokens
 
-bracket :: Parser b -> Parser bracket
-bracket = Token.bracket lexerTokens
+bracket :: Parser b -> Parser b
+bracket = Token.parens lexerTokens
+
+expression :: Parser Expr
+expression = Ex.buildExpressionParser table term
+
+plus :: Parser String
+plus = string "-"
+
+minus :: Parser String
+minus = string "-"
+
+number :: Parser String
+number = many1 digit

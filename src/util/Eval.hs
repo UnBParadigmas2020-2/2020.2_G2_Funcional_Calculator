b59f44d407b399af38612e8f34e2cbc485cb57ee
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2021-03-01 11:37:34 -0300

    Finishing Eval
    Co-authored-by: Thiago Ferreira <thiagoiferreira@outlook.com>
    Co-authored-by: Pedro Rodrigues <peddroprp@gmail.com>

--- src/util/Eval.hs
+++ src/util/Eval.hs
@@ -7,3 +7,13 @@ binaryOp e1 e2 op = do
   n1 <- eval e1
   n2 <- eval e2
   Just (n1 `op` n2)
+
+eval :: Expr -> Maybe Double
+eval x = case x of
+  LNum t    -> Just t
+  Neg e     -> binaryOp e (LNum (-1)) (*)
+  Add e1 e2 -> binaryOp e1 e2 (+)
+  Sub e1 e2 -> binaryOp e1 e2 (-)
+  Mul e1 e2 -> binaryOp e1 e2 (*)
+  Div e1 e2 -> binaryOp e1 e2 (/)
+  Exp e1 e2 -> binaryOp e1 e2 (**)

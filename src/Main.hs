0ce6c023a16df190ae047081c254cf2909ec6730
Pedro Rodrigues <peddroprp@gmail.com>
2021-03-01 11:55:44 -0300

    Add main
    Co-authored-by: Daniel de Sousa <daniel_sousa.unb@outlook.com>
    Co-authored-by: Thiago Ferreira <thiagoiferreira@outlook.com>

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import System.Console.Haskeline
+import Control.Monad.Trans
+import Text.Printf
+
+import Parser
+import Eval
+
+process :: String -> IO ()
+process line = do
+  let result = parser line
+  case result of
+    Left err -> print err
+    Right ex -> case eval ex of
+      Nothing -> putStrLn "Impossivel calcular"
+      Just result -> putStrLn $ printf "%.3f" result
+
 main :: IO ()
-main = putStrLn "Testing cabal"
+main = runInputT defaultSettings loop
+      where
+        loop = do
+          calcInput <- getInputLine "calc> "
+          case calcInput of
+            Nothing -> outputStrLn "Desligando..."
+            Just input -> liftIO (process input) >> loop
